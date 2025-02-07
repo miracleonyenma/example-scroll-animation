b40b0aee48806ca83ba339173b0bfb94775d1220
miracleonyenma <miracleficient@gmail.com>
2025-02-07 08:48:22 +0100

    feat: Implement scroll animation sections with framer-motion and update image configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,95 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import { motion, useInView } from "framer-motion";
+
+const SectionComponent: React.FC<{
+  title: string;
+  description: string;
+  image: string;
+}> = ({ title, description, image }) => {
+  const ref = useRef(null);
+
+  const isInView = useInView(ref, {
+    amount: 0.5,
+  });
+
+  useEffect(() => {
+    console.log("Element is in view: ", isInView);
+  }, [isInView]);
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: isInView ? 1 : 0 }}
+      ref={ref}
+      className="flex flex-col relative min-h-screen items-center justify-center gap-4 p-8 sm:p-16 font-[family-name:var(--font-geist-sans)]"
+    >
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        style={{
+          position: "fixed",
+          top: 0,
+          left: "0",
+          width: "100%",
+          height: "100%",
+        }}
+        animate={{
+          y: isInView ? 0 : 100,
+          opacity: isInView ? 1 : 0,
+        }}
+        exit={{ y: -100, opacity: 0 }}
+        transition={{
+          duration: 0.5,
+          ease: "easeInOut",
+        }}
+        className="flex gap-12 justify-center items-center"
+      >
+        <header>
+          <h1 className="text-4xl font-bold">{title}</h1>
+          <p className="text-lg">{description}</p>
+        </header>
+        <figure>
+          <Image
+            src={image}
+            alt="Section Image"
+            width={640}
+            height={480}
+            className="rounded-lg shadow-lg object-cover w-72 h-56"
+          />
+        </figure>
+      </motion.div>
+    </motion.section>
+  );
+};
 
 export default function Home() {
+  const [sections] = useState([
+    {
+      title: "Section 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      image:
+        "https://images.unsplash.com/photo-1738168346641-6aedb68a6c99?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "Section 2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      image:
+        "https://images.unsplash.com/photo-1736821481668-2cb07ceed73b?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "Section 3",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      image:
+        "https://images.unsplash.com/photo-1633158165793-c48be169c242?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ]);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main>
+      {sections.map((section, index) => (
+        <SectionComponent key={index} {...section} />
+      ))}
+    </main>
   );
 }
